b9523b89311c2eeaab491e8b42e8964a92fd112e
yigitKomsu <ihsanyigitergin@gmail.com>
2017-11-20 21:34:18 +0300

    Home page started

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'about', pathMatch: 'full'},
   { path: 'login', component: LoginComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'home', component: HomeComponent }
 ];
 
 
