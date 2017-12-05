e39b673dbbe5535f6f5e76a7b0199c17d9429388
Ege Apak <egeapak@gmail.com>
2017-12-05 17:29:30 +0300

    Change frontend build script to proper angular build

--- scripts/build_fe.sh
+++ scripts/build_fe.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 cd $FRONTEND_PATH
 mkdir -p /app/static
-$(npm bin)/ng build --prod --build-optimizer --deploy-url /static && cp -rf $FRONTEND_PATH/dist/* /app/static
+$(npm bin)/ng build --prod --build-optimizer --output-path /app/static --deploy-url /static
