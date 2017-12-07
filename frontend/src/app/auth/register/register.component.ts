7da5abd12544ef7b2ce0862872845f48b11ae655
Ege Apak <egeapak@gmail.com>
2017-12-07 19:34:14 +0300

    Send register information and show related alerts w/ redirection

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -1,16 +1,43 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { NotificationsService } from 'angular2-notifications';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss'],
-  encapsulation: ViewEncapsulation.None
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+    username: string;
+    password: string;
+    email: string;
 
-  ngOnInit() {
-  }
+    constructor(
+        private client: HttpClient, 
+        private notifications: NotificationsService,
+        private router: Router) { }
+
+    ngOnInit() {
+    }
+
+    register() {
+        this.client.post("/api/signup", {
+            username: this.username,
+            password: this.password,
+            email: this.email
+        }, {responseType: "text"}).subscribe(
+            resp => {
+                this.router.navigateByUrl("/login").then(_ =>{
+                    this.notifications.success("Register successful!", resp);
+                });
+            },
+            err => {
+                this.notifications.error("Couldn't Register", err.error);
+            }
+        )
+    }
 
 }
