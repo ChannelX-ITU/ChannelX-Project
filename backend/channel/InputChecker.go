bc805817480a4f33245e0d85c336603cccca1448
Safa Keskin <keskinsaf@itu.edu.tr>
2017-12-08 15:18:33 +0300

    InputChecker is completed

--- backend/channel/InputChecker.go
+++ backend/channel/InputChecker.go
@@ -7,31 +7,37 @@ import (
 	"github.com/go-ozzo/ozzo-validation/is"
 )
 
-var IsUserName = regexp.MustCompile(`^[a-zA-Z0-9_.-]+$`).MatchString
+var IsUserName 		= regexp.MustCompile(`^[a-zA-Z0-9_.-]+$`)
+var IsPhoneNumber	= regexp.MustCompile( `(0|\\+90|090|90)[0-9]+$`)
 
 var(
-	ErrInvalidEmailOnSignup   	= ChannelError{"Err_Invalid_Email_On_Signup", "E-mail address is not valid", http.StatusBadRequest}
+	ErrInvalidEmail			   	= ChannelError{"Err_Invalid_Email", "E-mail address is not valid", http.StatusBadRequest}
 	ErrInvalidUsernameOnSignup	= ChannelError{"Err_Invalid_Username_On_Signup", "Username is not valid", http.StatusBadRequest}
 	ErrInvalidPasswordOnSignup	= ChannelError{"Err_Invalid_Password_On_Signup", "Password is not valid", http.StatusBadRequest}
 	ErrInvalidChannelNameLength	= ChannelError{"Err_Invalid_Channel_Name_Length", "Channel name length is not suitable", http.StatusBadRequest}
 	ErrInvalidChannelName		= ChannelError{"Err_Invalid_Channel_Name", "Invalid channel name (Same character restrictions with username)", http.StatusBadRequest}
-	ErrInvalidCommType			= ChannelError{"Err_Invalid_Comm_Type", "Invalid Communicaton Type", http.StatusBadRequest}
+	ErrInvalidCommType			= ChannelError{"Err_Invalid_Comm_Type", "Invalid Communication Type", http.StatusBadRequest}
 	ErrInvalidAlias				= ChannelError{"Err_Invalid_Alias", "Invalid Alias (Same character restrictions with username)", http.StatusBadRequest}
+	ErrEmptyChannelPreference	= ChannelError{"Err_Empty_Channel_Preference", "Invalid Alias ", http.StatusBadRequest}
+	ErrEmptyChannelRestrictions = ChannelError{"Err_Empty_Channel_Restrictions", "Channel Restrictions is not set.", http.StatusBadRequest}
+	ErrInvalidPhoneNumber		= ChannelError{"Err_Invalid_Phone_Number", "Phone number must be valid.", http.StatusBadRequest}
 )
 
 
-func ( s *Server ) IsLoginValid ( login Login ) *ChannelError {
-	if IsUserName( login.Username ) == false || len(login.Password) < 8 || len(login.Password) > 16  {
+func ( s *Server ) LoginValidation ( login Login ) *ChannelError {
+	if validation.Validate( login.Username, validation.Required,
+		validation.Match( IsUserName ), validation.Length(3, 16) ) != nil {
 		return &ErrInvalidLoginCredentials
 	}
 	return nil
 }
 
-func ( s *Server ) IsSignupValid ( signup SignUp ) *ChannelError {
+func ( s *Server ) SignupValidation ( signup SignUp ) *ChannelError {
 	if validation.Validate(signup.Email, validation.Required, is.Email) != nil {
-		return &ErrInvalidEmailOnSignup
+		return &ErrInvalidEmail
 	}
-	if IsUserName( signup.Username ) == false {
+	if validation.Validate( signup.Username, validation.Required,
+		validation.Match( IsUserName ), validation.Length(3, 16) ) != nil {
 		return &ErrInvalidUsernameOnSignup
 	}
 	if validation.Validate( signup.Password, validation.Required, validation.Length(6, 50) ) != nil {
@@ -40,20 +46,61 @@ func ( s *Server ) IsSignupValid ( signup SignUp ) *ChannelError {
 	return nil
 }
 
-func ( s *Server ) CanJoinChannel ( joinchannel JoinChannel ) *ChannelError {
-	if len( joinchannel.Channel ) < 4 || len(joinchannel.Channel) > 30 {
+func ( s *Server ) JoinChannelValidation ( joinchannel JoinChannel ) *ChannelError {
+	if validation.Validate( joinchannel.Channel, validation.Required, validation.Length(4, 30) ) != nil {
 		return &ErrInvalidChannelNameLength
 	}
-	if IsUserName( joinchannel.Channel ) {
+	if validation.Validate( joinchannel.Channel, validation.Required,
+		validation.Match( IsUserName ), validation.Length(3, 16) ) != nil {
 		return &ErrInvalidChannelName
 	}
 	if joinchannel.Comm != `SMS`  && joinchannel.Comm != `EMAIL` {
 		return &ErrInvalidCommType
 	}
-	if  len( joinchannel.Alias ) != 0 {
-		if IsUserName(joinchannel.Alias) == false {
-			return &ErrInvalidAlias
-		}
+	if validation.Validate( joinchannel.Alias, validation.Required, validation.Match( IsUserName ),
+		validation.Length(3, 16) ) != nil {
+		return &ErrInvalidAlias
+	}
+	return nil
+}
+
+func ( s *Server ) AddChannelValidation ( addchannel AddChannel ) *ChannelError {
+	if validation.Validate(addchannel.Channel.Name, validation.Required, validation.Match(IsUserName),
+		validation.Length(4, 50)) != nil {
+		return &ErrInvalidChannelName
+	}
+
+	if &(addchannel.Channel.Preference) == nil {
+		return &ErrEmptyChannelPreference
+	}
+	if len(addchannel.Channel.Restrictions) == 0 {
+		return &ErrEmptyChannelRestrictions
+	}
+	if addchannel.Comm != `SMS`  && addchannel.Comm != `EMAIL` {
+		return &ErrInvalidCommType
+	}
+
+	return nil
+}
+
+func ( s *Server ) AddCommValidation ( comm AddComm ) *ChannelError{
+	if comm.CommType != `SMS`  && comm.CommType != `EMAIL` {
+		return &ErrInvalidCommType
+	}
+	if comm.CommType == `SMS` && validation.Validate( comm.Comm, validation.Required, is.Digit,
+		validation.Match( IsPhoneNumber ) ) != nil {
+		return &ErrInvalidPhoneNumber
+	}
+	if comm.CommType == `EMAIL` && validation.Validate( comm.Comm, validation.Required, is.Email ) != nil {
+		return &ErrInvalidEmail
+	}
+	return nil
+}
+
+func ( s *Server ) DeleteCommValidation ( comm DeleteComm ) *ChannelError{
+	if validation.Validate( comm.Comm, validation.Required, is.Email ) != nil &&
+		validation.Validate(comm.Comm, validation.Required, validation.Match(IsPhoneNumber) ) != nil {
+		return &ErrInvalidCommType
 	}
 	return nil
 }
