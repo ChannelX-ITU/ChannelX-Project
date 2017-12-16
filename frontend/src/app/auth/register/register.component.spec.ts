d431f9ac8495a49c11e1dd313bde02c13809fab5
yigitKomsu <ihsanyigitergin@gmail.com>
2017-12-16 23:09:23 +0300

    Revert "Started writing test codes"
    This reverts commit d78a0c3b3dd76738605d8cf10b15a25d16037965.

--- frontend/src/app/auth/register/register.component.spec.ts
+++ frontend/src/app/auth/register/register.component.spec.ts
@@ -5,12 +5,9 @@ import { RegisterComponent } from './register.component';
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
-  let debug: DebugElement;
-  let element: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ HttpModule ],
       declarations: [ RegisterComponent ]
     })
     .compileComponents();
@@ -19,37 +16,10 @@ describe('RegisterComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
-    debug = fixture.debugElement.query(By.css('form');
-    element = debug.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  //it should get a string from Username
-  it('should get a string from', () => {
-    expect(element.username).toBe(typeof(text));
-  });
-
-  //it should get a string from email (maybe check for validity too?)
-  it('should get a string from', () => {
-    expect(element.email).toBe(typeof(text));
-  });
-
-  //it should get a string from password
-  it('should get a string from', () => {
-    expect(element.password).toBe(typeof(text));
-  });
-
-  //it should send a register request when register button is clicked
-  it('should send register request when clicked', () => {
-    let btn = element.submit;
-    btn.triggerEventHandler('click', null);
-    component.register().subscribe(x => {
-      expect(x).toContain(true); //I don't understand this
-    });
-  });
-
 });
